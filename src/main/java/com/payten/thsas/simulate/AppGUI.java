2cf5bfe26ff910338bdbee6b0aaf8a3b44fdefb8
Predrag-Dimitrijevic <Predrag.Dimitrijevic2@payten.com>
2024-11-18 08:02:03 +0100

    main var was not used

--- src/main/java/com/payten/thsas/simulate/AppGUI.java
+++ src/main/java/com/payten/thsas/simulate/AppGUI.java
@@ -4,6 +4,6 @@ import com.payten.thsas.simulate.gui.MainWindow;
 
 public class AppGUI {
   public static void main(String[] args) {
-    MainWindow main = new MainWindow();
+    new MainWindow();
   }
 }
